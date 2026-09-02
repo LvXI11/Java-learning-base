9182dd20840b75478f833822343fd960f98651f3
Lv <releaseinstinct7@qq.com>
2026-09-02 20:11:25 +0800

    第二天练习代码

---
+++ 02-exercise/String_concatenate/src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args){
+        String a="hello";
+        int b=123;
+        double c=12.23;
+        int d=98;
+        System.out.println(a+" "+"world"+c);
+        System.out.println(d+'a'+a+" "+c);
+    }
+}
---
+++ 02-exercise/type_conversion/src/test.java
@@ -0,0 +1,20 @@
+public class test {
+    public static void main(String[] args){
+        //强转丢失精度
+//        int a=12;
+//        float b=3.2f;
+//        int c=(int)b;
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+
+//        int a=5;
+//        int b=2;
+//        double c=a/b;
+//        System.out.println(c);
+        int a=Integer.parseInt("343");
+        double b=Double.parseDouble(".3");
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
