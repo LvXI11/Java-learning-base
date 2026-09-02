011feb88b707e820de43da620f526842b3f6406b
Lv <releaseinstinct7@qq.com>
2026-09-02 08:53:32 +0800

    first commit

---
+++ 01-exercise/src/Main.java
@@ -0,0 +1,12 @@
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+        String name="Lv";
+        int age=19;
+        double weight=130.2;
+        boolean isStudent=true;
+        System.out.println("name:"+name+"\n"+"age:"+age+"\n"+"weight:"+"\n"
+        +"Student:"+isStudent);
+    }
+}
